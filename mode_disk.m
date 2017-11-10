619f24cd9245b7667a91bfe70fe482148091bdc8
Camilo Sampedro <sampedro1903@gmail.com>
2017-11-09 22:19:58 -0500

    New functions added

--- mode_disk.m
+++ mode_disk.m
@@ -1,13 +1,19 @@
-function move_disk( hanoi_pins, source_pin, destination_pin )
+function [ hanoi_pins ] = move_disk( hanoi_pins, source_pin, destination_pin )
 % Moves a pin from a pin to another pin
 %   Detailed explanation goes here
-disk_index = get_higher_disk(hanoi_pins(:, source_pin));
-
+[ disk, disk_index ] = min(hanoi_pins(:, source_pin));
+hanoi_pins(disk_index, source_pin) = 0;
+hanoi_pins(:, destination_pin) = put_disk(hanoi_pins(:, destination_pin), disk);
 end
 
-function [ disk_index ] = get_higher_disk_position(pin)
+function [ pin ] = put_disk(pin, disk)
+[ min_disk, disk_index ] = min(pin);
+if (disk < min_disk)
+    % Cannot move this disk, the destination pin has a smaller disk on top
 end
-
-function put_disk(pin, disk)
+% if (disk < 1) SHOULD NOT happen
+    % Cannot move this disk, there is not enough space
+% end
+pin(disk_index - 1) = disk;
 end
 
