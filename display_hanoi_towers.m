f11371b210f6cd29b4238bc99784123bfbdc08b6
Camilo Sampedro <sampedro1903@gmail.com>
2017-11-13 13:51:46 -0500

    Comments added

--- display_hanoi_towers.m
+++ display_hanoi_towers.m
@@ -1,22 +1,38 @@
 function display_hanoi_towers( hanoi_towers )
-%DISPLAY_HANOI_TOWERS Summary of this function goes here
-%   Detailed explanation goes here
-header = {'Pin_1','Pin_2','Pin_3'};
+%DISPLAY_HANOI_TOWERS Beautify and print the hanoi towers
+%   Convert the numbers inside the hanoi towers into disks of that size,
+%   then prints it to the user
+% Number of rows and columns, to iterate inside the hanoi towers
 [nr, nc] = size(hanoi_towers);
+% Initialize the beauty hanoi towers with space strings of size nr, which is then
+% maximum disk size
 beauty_disks = repmat({repmat(' ', 1, nr)}, nr, nc);
+% Iterate through rows
 for r = 1:nr
+    % and columns
     for c = 1:nc
+        % Extract the original disk size, inside the hanoi_towers
         disk_size = hanoi_towers(r,c);
+        % intmax('uint8') is the value used to recognize the values that are not
+        % disks, if the disk_size is intmax('uint8'), it means that it was not a
+        % disk, so the program will print an empty bar: '|'
         if(disk_size == intmax('uint8'))
             disk = '|';
-        else
-            disk = repmat('=', 1, hanoi_towers(r,c));
+        else % If it was indeed a disk
+            % Create a string with '=', repeated the disk_size times. e.g. if
+            % disk size is 3, the disk will be '==='
+            disk = repmat('=', 1, disk_size);
         end
+        % Asign this beauty disk to the beauty disks matrix
         beauty_disks(r,c) = {disk};
     end
 end
+% The headers are labels that go below each tower
+header = {'Pin_1','Pin_2','Pin_3'};
+% Concatenate the beauty disks with their headers below
 disp_hanoi = [beauty_disks; header];
+% Simple message to tell the user that their hanoi towers will be printed
 disp('These are your hanoi towers: ');
+% And then show the Beauty Hanoi Towers
 disp(disp_hanoi);
 end
-
--- have_won.m
+++ have_won.m
@@ -1,13 +1,18 @@
 function [ response ] = have_won( hanoi_towers )
-%HAVE_WON Summary of this function goes here
-%   Detailed explanation goes here
+%HAVE_WON Check if the hanoi_towers have a winning composition
+%   Checks if the right tower is completely filled with disks
+% Number of disks in the Hanoi Towers (Rows)
 [nr, ~] = size(hanoi_towers);
+% Iterate through the disks on the last pin
 for r = 1:nr
+    % If the last tower contains an space that is not filled with a disk (a number
+    % different to intmax('uint8')), the user has not won.
     if(hanoi_towers(r,3) == intmax('uint8'))
         response = false;
+        % Break the loop here, because we already know that the user has not won.
         return;
     end
 end
+% If all of the spaces on the right tower were disks, the user has won
 response = true;
 end
-
