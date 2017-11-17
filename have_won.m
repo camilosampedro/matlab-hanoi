b0d8ae677e4a616d564b98254d2d6e229cec88fa
Camilo Sampedro <sampedro1903@gmail.com>
2017-11-16 20:01:00 -0500

    hanoi_towers are now hanoi_pins

--- have_won.m
+++ have_won.m
@@ -1,13 +1,13 @@
-function [ response ] = have_won( hanoi_towers )
+function [ response ] = have_won( hanoi_pins )
 %HAVE_WON Check if the hanoi_towers have a winning composition
 %   Checks if the right tower is completely filled with disks
 % Number of disks in the Hanoi Towers (Rows)
-[nr, ~] = size(hanoi_towers);
+[nr, ~] = size(hanoi_pins);
 % Iterate through the disks on the last pin
 for r = 1:nr
     % If the last tower contains an space that is not filled with a disk (a number
     % different to intmax('uint8')), the user has not won.
-    if(hanoi_towers(r,3) == 1000)
+    if(hanoi_pins(r,3) == 1000)
         response = false;
         % Break the loop here, because we already know that the user has not won.
         return;
