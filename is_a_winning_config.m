f34fe0316f5bb7afbed5356e4e31b81fa85aa9e1
Camilo Sampedro <sampedro1903@gmail.com>
2017-11-16 20:21:58 -0500

    rows -> row

--- is_a_winning_config.m
+++ is_a_winning_config.m
@@ -4,10 +4,10 @@ function [ have_won ] = is_a_winning_config( hanoi_towers )
 % Number of disks in the Hanoi Towers (Rows)
 [number_of_rows, ~] = size(hanoi_towers);
 % Iterate through the disks on the last pin
-for rows = 1:number_of_rows
+for row = 1:number_of_rows
     % If the last tower contains an space that is not filled with a disk (a number
     % different to intmax('uint8')), the user has not won.
-    if(hanoi_towers(rows, 3) == intmax('uint8'))
+    if(hanoi_towers(row, 3) == intmax('uint8'))
         have_won = false;
         % Break the loop here, because we already know that the user has not won.
         return;
