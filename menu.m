527a4c0ef54d2da59e4a273439f8ed8a8af9efbd
Camilo A. Sampedro Restrepo <sampedro1903@gmail.com>
2017-11-23 09:01:16 -0500

    Update menu.m

--- menu.m
+++ menu.m
@@ -2,7 +2,7 @@ function menu( hanoi_pins )
 %MENU Menu
 %   Repeat movements while the user has not won
 while (~have_won(hanoi_pins))
-    hanoi_pins = user_move_disk(hanoi_pins);
+    hanoi_pins = move_disk(hanoi_pins);
 end
 end
 
